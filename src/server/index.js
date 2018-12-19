0bbc2bc7924c2f61f86b1cff613e9c2a5e6d511c
Your Name <wlodziuss@wp.pl>
2018-12-19 17:36:00 +0100

    Initial commit

---
+++ src/server/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const os = require('os');
+
+const app = express();
+
+
+app.get('/api/getUsername', (req, res) => res.send({ username: "test" }));
+app.listen(12345, () => console.log('Listening on port 12345!'));
